cee911893b2838b36139d621209d2b68cc7460a7
Kevin Doolaeghe <doolaeghe.kevin@gmail.com>
2024-12-29 14:47:08 +0100

    updated

--- init.sh
+++ init.sh
@@ -1,30 +1,136 @@
 #!/bin/bash
 
-# Function to check if Docker is running with proper permissions
-check_docker_permissions() {
+# Function to check permissions
+check_permissions() {
     if ! sudo -v; then
         echo "You need sudo privileges to run Docker commands. Please ensure you have proper permissions."
         exit 1
     fi
+}
 
-    if ! groups $(whoami) | grep -q "\bdocker\b"; then
-        echo "You are not in the Docker group. Adding you to the Docker group..."
-        sudo usermod -aG docker $(whoami)
-        echo "You have been added to the Docker group. Please log out and log back in, or run 'newgrp docker' to apply changes."
+# Function to read input with regex validation
+read_with_regex() {
+    local prompt="$1"
+    local regex="$2"
+    local default="$3"
+    local input
+
+    while true; do
+        # Read user input
+        if [[ -n $default ]]; then
+            read -p "$prompt (default: $default): " input
+            # Use default if input is empty
+            input="${input:-$default}"
+        else
+            read -p "$prompt: " input
+        fi
+
+        # Trim spaces from both ends
+        input = $(echo "$input" | xargs)
+
+        # Check if the input is valid
+        if [[ $input =~ $regex ]]; then
+            # Return valid input
+            echo "$input"
+            return
+        else
+            echo "Invalid input. Please try again."
+        fi
+    done
+}
+
+# Function to set environment variable
+set_environment_variable() {
+    local var_name="$1"
+    local var_value="$2"
+    local env_file="/etc/environment"
+
+    # Check if the variable is already in file
+    if grep -Eq "^[^#]*\b${var_name}=" "$env_file"; then
+        # Update the value if it's different
+        if [ "$(sudo grep -E "^[^#]*\b${var_name}=" "$env_file" | cut -d'=' -f2-)" != "$var_value" ]; then
+            sudo sed -i "s#^${var_name}=.*#${var_name}=${var_value}#" "$env_file"
+            echo "Updated $var_name in $env_file"
+        fi
+    else
+        # Append the new variable
+        echo "${var_name}=${var_value}" | sudo tee -a "$env_file" > /dev/null
+        echo "Added $var_name to $env_file"
+    fi
+
+    # Export for current session
+    export "${var_name}=${var_value}"
+}
+
+# Function to set environment variable with command
+set_environment_variable_with_command() {
+    local var_name="$1"
+    local command="$2"
+    local var_value
+
+    # Check if the variable is already set
+    if [ -z "${!var_name}" ]; then
+        var_value="${command}"
+        set_environment_variable "$var_name" "$var_value"
+    fi
+}
+
+# Function to set environment variable with prompt and regex
+set_environment_variable_with_prompt_and_regex() {
+    local var_name="$1"
+    local prompt="$2"
+    local regex="$3"
+    local default="$4"
+    local var_value
+
+    # Check if the variable is already set
+    if [ -z "${!var_name}" ]; then
+        var_value=$(read_with_regex "$prompt" "$regex" "$default")
+        set_environment_variable "$var_name" "$var_value"
+    fi
+}
+
+# Function to set Docker secret
+set_docker_secret() {
+    local secret_name="$1"
+    local secret_value="$2"
+
+    # Create the Docker secret
+    echo "$secret_value" | sudo docker secret create "$secret_name" - || {
+        echo "Failed to create Docker secret '$secret_name'."
         exit 1
+    }
+    echo "Docker secret '$secret_name' created."
+}
+
+# Function to set Docker secret with prompt and regex
+set_docker_secret_with_prompt_and_regex() {
+    local secret_name="$1"
+    local prompt="$2"
+    local regex="$3"
+    local default="$4"
+    local secret_value
+
+    # Check if the secret is already set
+    if ! sudo docker secret ls | grep -w "$secret_name" > /dev/null 2>&1; then
+        secret_value=$(read_with_regex "$prompt" "$regex" "$default")
+        set_docker_secret "$secret_name" "$secret_value"
     fi
 }
 
 # Function to install Docker
-setup_docker() {
+install_docker() {
+    # Check if Docker is installed
     command -v docker > /dev/null 2>&1 || {
+        # Install Docker
         echo "Docker is not installed. Installing Docker..."
-
         if ! curl -fsSL https://get.docker.com | sudo sh; then
             echo "Failed to install Docker. Please check your system's configuration."
             exit 1
         fi
+        echo "Docker has been installed."
 
+        # Start Docker service
         if ! sudo systemctl is-active --quiet docker; then
             echo "Docker service is not running. Starting Docker..."
             sudo systemctl start docker || {
@@ -32,34 +138,24 @@ setup_docker() {
                 exit 1
             }
         fi
-
-        echo "Docker has been installed."
+        echo "Docker service is running."
     }
 }
 
 # Function to initialize Docker Swarm
 initialize_docker_swarm() {
+    local default_addr_pool
+    local advertise_addr
+
+    # Check if Docker Swarm is initialized
     sudo docker info | grep -i swarm > /dev/null 2>&1 || {
         echo "Docker Swarm is not initialized. Initializing Docker Swarm..."
 
-        while true; do
-            read -p "Enter the default address pool (e.g., 10.10.0.0/16): " default_addr_pool
-            if [[ $default_addr_pool =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}$ ]]; then
-                break
-            else
-                echo "Invalid address pool. Please try again."
-            fi
-        done
+        # Read the default address pool and advertise address for Docker Swarm
+        default_addr_pool=$(read_with_regex "Enter the default address pool" "^([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}\$" "10.10.0.0/16")
+        advertise_addr=$(read_with_regex "Enter the advertise address" "^([0-9]{1,3}\.){3}[0-9]{1,3}\$" "10.0.0.252")
 
-        while true; do
-            read -p "Enter the advertise address (e.g., 10.0.0.252): " advertise_addr
-            if [[ $advertise_addr =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
-                break
-            else
-                echo "Invalid IP address. Please try again."
-            fi
-        done
-
+        # Initialize Docker Swarm
         sudo docker swarm init --default-addr-pool $default_addr_pool --advertise-addr $advertise_addr || {
             echo "Failed to initialize Docker Swarm."
             exit 1
@@ -70,175 +166,83 @@ initialize_docker_swarm() {
 
 # Function to create a system user for Docker
 create_docker_user() {
-    docker_user="dockeruser"
-    if ! id -u $docker_user > /dev/null 2>&1; then
-        echo "Creating system user '$docker_user'..."
+    local docker_user="dockeruser"
 
+    # Check if the system user exists
+    if ! id -u $docker_user > /dev/null 2>&1; then
+        # Create a new user
+        echo "Creating system user '$docker_user'..."
         sudo useradd -m -s /bin/bash $docker_user || {
             echo "Failed to create user '$docker_user'."
             exit 1
         }
+        echo "User '$docker_user' created."
 
+        # Set a password for the user
         echo "Set a password for '$docker_user':"
         sudo passwd $docker_user
 
-        if ! groups $docker_user | grep -q "\bdocker\b"; then
-            sudo usermod -aG docker $docker_user
-        fi
+        # Set the 'DOCKER_PUID' environment variable
+        echo "Updating DOCKER_PUID environment variable..."
+        set_environment_variable_with_command "DOCKER_PUID" "$(id -u dockeruser)"
+    fi
+
+    # Check if the user is in the 'docker' group
+    if ! groups $docker_user | grep -q "\bdocker\b"; then
+        # Add the user to the 'docker' group
+        echo "User '$docker_user' is not in the 'docker' group. Adding user to the 'docker' group..."
+        sudo usermod -aG docker $docker_user
+        echo "User '$docker_user' added to the 'docker' group."
         
-        echo "User '$docker_user' created and added to the 'docker' group."
+        # Set the 'DOCKER_PGID' environment variable
+        echo "Updating DOCKER_PGID environment variable..."
+        set_environment_variable_with_command "DOCKER_PGID" "$(getent group docker | cut -d: -f3)"
     fi
 }
 
 # Function to initialize Docker secrets
 initialize_docker_secrets() {
-    initializing_secrets=false
+    echo "Checking for existing Docker secrets..."
 
-    declare -A secrets=( 
-        ["duckdns_token"]="Duck DNS token"
-        ["username"]="username"
-        ["password"]="password"
-    )
-
-    for secret in "${!secrets[@]}"; do
-        if ! sudo docker secret ls | grep -w "$secret" > /dev/null 2>&1; then
-            if ! $initializing_secrets; then
-                echo "Initializing Docker secrets..."
-                initializing_secrets=true
-            fi
-
-            while true; do
-                read -s -p "Enter the ${secrets[$secret]}: " secret_value
-                if [ -n "$secret_value" ]; then
-                    echo "$secret_value" | sudo docker secret create "$secret" - || {
-                        echo "Failed to create Docker secret '$secret'."
-                        exit 1
-                    }
-                    echo "Docker secret '$secret' created."
-                    break
-                else
-                    echo "${secrets[$secret]} cannot be empty. Please try again."
-                fi
-            done
-        fi
-    done
-
-    if $initializing_secrets; then
-        echo "Docker secrets initialized."
-    fi
+    set_docker_secret_with_prompt_and_regex "duckdns_token" "Enter the Duck DNS token" "^[a-zA-Z0-9]{32}\$"
+    set_docker_secret_with_prompt_and_regex "username" "Enter the username" "^[a-zA-Z0-9]{4,}\$"
+    set_docker_secret_with_prompt_and_regex "password" "Enter the password" "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@\$!%*?&])[A-Za-z\d@\$!%*?&]{8,20}\$"
 }
 
 # Function to set environment variables for Docker
 set_docker_environment_variables() {
-    initializing_env_vars=false
+    echo "Checking for Docker environment variables..."
 
-    declare -A env_vars_1=( 
-        [DOCKER_PUID]=$(id -u dockeruser)
-        [DOCKER_PGID]=$(getent group docker | cut -d: -f3)
-        [DOCKER_TZ]=$(cat /etc/timezone 2>/dev/null || timedatectl | grep "Time zone" | awk '{print $3}')
-    )
+    set_environment_variable_with_command "DOCKER_TZ" "$(cat /etc/timezone 2>/dev/null || timedatectl | grep "Time zone" | awk '{print $3}')"
 
-    for var in "${!env_vars_1[@]}"; do
-        if [ -z "${!var}" ]; then
-            if ! $initializing_env_vars; then
-                echo "Initializing Docker environment variables..."
-                initializing_env_vars=true
-            fi
-
-            echo "Setting $var environment variable..."
-            env_var_value="${env_vars_1[$var]}"
-            export "$var"="$env_var_value"
-            if ! grep -q "^export $var=" ~/.bashrc; then
-                echo "export $var=$env_var_value" >> ~/.bashrc
-            fi
-
-            echo "$var set to ${!var}."
-        fi
-    done
-
-    declare -A env_vars_2=( 
-        [WIREGUARD_DOMAIN_NAME]="domain name used for WireGuard VPN"
-        [TRAEFIK_DOMAIN_NAME]="domain name used for Traefik reverse-proxy"
-    )
-
-    for var in "${!env_vars_2[@]}"; do
-        if [ -z "${!var}" ]; then
-            if ! $initializing_env_vars; then
-                echo "Initializing Docker environment variables..."
-                initializing_env_vars=true
-            fi
-
-            while true; do
-                read -p "Enter the ${env_vars_2[$var]}: " env_var_value
-                if [[ "$env_var_value" =~ ^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
-                    echo "Setting $var environment variable..."
-                    export "$var"="$env_var_value"
-                    if ! grep -q "^export $var=" ~/.bashrc; then
-                        echo "export $var=$env_var_value" >> ~/.bashrc
-                    fi
-                    echo "$var set to ${!var}."
-                    break
-                else
-                    echo "Invalid domain format. Please try again."
-                fi
-            done
-        fi
-    done
-    
-    declare -A env_vars_3=( 
-        [DUCKDNS_EMAIL]="Duck DNS email address"
-    )
-
-    for var in "${!env_vars_3[@]}"; do
-        if [ -z "${!var}" ]; then
-            if ! $initializing_env_vars; then
-                echo "Initializing Docker environment variables..."
-                initializing_env_vars=true
-            fi
-
-            while true; do
-                read -p "Enter the ${env_vars_3[$var]}: " env_var_value
-                if [[ "$env_var_value" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
-                    echo "Setting $var environment variable..."
-                    export "$var"="$env_var_value"
-                    if ! grep -q "^export $var=" ~/.bashrc; then
-                        echo "export $var=$env_var_value" >> ~/.bashrc
-                    fi
-                    echo "$var set to ${!var}."
-                    break
-                else
-                    echo "Invalid email address format. Please try again."
-                fi
-            done
-        fi
-    done
-
-    if $initializing_env_vars; then
-        echo "Docker environment variables initialized."
-    fi
+    set_environment_variable_with_prompt_and_regex "WIREGUARD_DOMAIN_NAME" "Enter the domain name used for WireGuard VPN" "^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\$"
+    set_environment_variable_with_prompt_and_regex "TRAEFIK_DOMAIN_NAME" "Enter the domain name used for Traefik reverse-proxy" "^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\$"
+    set_environment_variable_with_prompt_and_regex "DUCKDNS_EMAIL" "Enter the Duck DNS email address" "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\$"
 }
 
 # Function to create the Docker network
 create_docker_network() {
-    DOCKER_NETWORK="docker_network"
-    if ! sudo docker network inspect $DOCKER_NETWORK > /dev/null 2>&1; then
-        echo "Creating $DOCKER_NETWORK network..."
-        sudo docker network create --driver overlay --attachable $DOCKER_NETWORK || {
-            echo "Failed to create Docker network '$DOCKER_NETWORK'."
+    local docker_network="docker_network"
+    
+    # Check if the Docker network exists
+    if ! sudo docker network inspect $docker_network > /dev/null 2>&1; then
+        # Create the Docker network
+        echo "Creating $docker_network network..."
+        sudo docker network create --driver overlay --attachable $docker_network || {
+            echo "Failed to create Docker network '$docker_network'."
             exit 1
         }
-        echo "$DOCKER_NETWORK network created."
+        echo "$docker_network network created."
 
-        echo "Setting DOCKER_NETWORK environment variable..."
-        export DOCKER_NETWORK
-        echo "export DOCKER_NETWORK=$DOCKER_NETWORK" >> ~/.bashrc
-        echo "DOCKER_NETWORK set to $DOCKER_NETWORK."
+        # Set the 'DOCKER_NETWORK' environment variable
+        echo "Updating DOCKER_NETWORK environment variable..."
+        set_environment_variable "DOCKER_NETWORK" "$docker_network"
     fi
 }
 
 # Main script execution
-check_docker_permissions
-setup_docker
+check_permissions
+install_docker
 initialize_docker_swarm
 create_docker_user
 initialize_docker_secrets
